4808dd99f9043625ce6f3ae183bb7a50bbcf6c6e
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-08-03 12:53:01 -0300

    Create 10deltas.py

---
+++ 10deltas.py
@@ -0,0 +1,15 @@
+'''03/08/2020 - jonatan paschoal'''
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import timedelta
+
+def deltaTempo():
+    delta = timedelta(86, hours=8532, minutes = 7645)
+    print(delta)
+
+    hoje = datetime.now()
+    print('Data no futuro: ', hoje + delta)
+    print('Data no passado: ', hoje - delta)
+
+deltaTempo()
