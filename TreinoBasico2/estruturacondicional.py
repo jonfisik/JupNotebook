0f45cf43fb3b98187d509acd93aa659a17b7be31
Jonatan Pantoja Paschoal <development.wj@outlook.com>
2020-08-03 10:42:20 -0300

    Create estruturacondicional.py

---
+++ TreinoBasico2/estruturacondicional.py
@@ -0,0 +1,13 @@
+'''03/08/2020 - Jonatan Paschoal'''
+
+def Condicional():
+    x, y = 100000, 100000
+
+    if (x < y):
+        print('x é menor do que y.')
+    elif(x == y):
+        print('x é igual a y.')
+    else:
+        print('x é maior do y.')
+
+Condicional()
